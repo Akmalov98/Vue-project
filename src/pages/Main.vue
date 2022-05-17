4b29b231c47e475de693b2baca9408b842cffc98
Akbar <oscarbroflovsky@gmail.com>
2022-05-17 21:08:33 +0300

    Initial commit

---
+++ src/pages/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Добро пожаловать на практическую работу по Vue 3</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
